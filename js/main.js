241c26b68855a0fbdb866a993a2c1334a9a4bae9
宇都宮靖之 <utsunomiyayasuyuki@utsunomiyayasuyukinoMacBook-Pro.local>
2020-01-29 10:02:04 -0800

    second_commit

--- js/main.js
+++ js/main.js
@@ -4,19 +4,43 @@
     const btn = document.getElementById('btn');
 
     btn.addEventListener('click', ()=>{
-        const n = Math.floor(Math.random() *3);
-        btn.textContent = n;
+        // 確率操作の方法；大吉を出やすくする
+        // const results = ['大吉','大吉','大吉','大吉','凶'];
 
-        switch(n) {
-            case 0:
-            btn.textContent = '大吉';
-            break;
-            case 1:
-            btn.textContent = '中吉';
-            break;
-            case 2:
-            btn.textContent = '凶';
-            break;
-        }
+        const results = ['大吉','中吉','凶','末吉'];
+
+        const n = Math.floor(Math.random() * results.length);
+         
+        btn.textContent = results[n];
+
+        // nを用いない方法
+        // btn.textContent = results[Math.floor(Math.random() * results.length)];
+
+        
+        // 確率操作の方法
+        // const n = Math.random();
+        // if(n < 0.05){
+        //     btn.textContent = '大吉';　5%の確率で大吉
+        // }else if(n < 0.2){
+        //     btn.textContent = '中吉';　15%の確率で中吉
+        // }else {
+        //     btn.textContent = '凶'; 80%の確率で凶
+        // }
+
+
+        
+        // switchを用いた書き方
+        // switch(n) {
+        //     case 0:
+        //     btn.textContent = '大吉';
+        //     break;
+        //     case 1:
+        //     btn.textContent = '中吉';
+        //     break;
+        //     case 2:
+        //     btn.textContent = '凶';
+        //     break;
+        // }
     });
 }
+
